518ede169f4c08e29db59538f5e3e5694e4ae75e
alaa1124 <69214603+alaa1124@users.noreply.github.com>
2024-08-08 12:29:23 +0300

    model creation

--- __manifest__.py
+++ __manifest__.py
@@ -18,13 +18,14 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'hr'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+         'views/sequence_data.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/models.py
+++ models/models.py
@@ -1,19 +1,41 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class attendance_modificationreq(models.Model):
-#     _name = 'attendance_modificationreq.attendance_modificationreq'
-#     _description = 'attendance_modificationreq.attendance_modificationreq'
+class attendance_modificationreq(models.Model):
+    _name = 'attendance_modificationreq.attendance_modificationreq'
+    _description = 'attendance_modificationreq.attendance_modificationreq'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(string='Reference', required=True, copy=False, readonly=True, default='New')
+    employeename = fields.Many2one('hr.employee', string='Employee', required=True)
+    Date = fields.Date(string='Date')
+    type = fields.Selection([
+        ('check_in', 'Check In'),
+        ('check_out', 'Check Out')
+    ], string='Type', required=True)
+    action = fields.Selection([
+        ('new_record', 'New Record'),
+        ('modifcation', 'Modifcation')
+    ], string='Type', required=True)
+    reason = fields.Text()
+    attendance = fields.Many2one(
+        'hr.attendance',
+        string='Attendance',
+        domain=lambda self: [('employee_id', '=', self.employeename.id)],
+        states={'draft': [('readonly', False)]},
+        attrs={'invisible': [('state', '!=', 'draft')]}
+    )
+
+    
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', 'New') == 'New':
+            vals['name'] = self.env['ir.sequence'].next_by_code('attendance_modificationreq.seq') or 'New'
+        return super(attendance_modificationreq, self).create(vals)
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
 
